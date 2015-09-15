dc4d1e66f5e5c91966f0fbccd319cf60010f3c4c
Catherine Leung <cathyatseneca@gmail.com>
2015-09-15 11:07:59 -0400

    input examples

---
+++ input1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void){
+	int userInput;
+	//output statement
+	printf("please enter a number: ");
+	
+	//input statement
+	scanf("%d", &userInput);	
+
+	printf("The number entered was : %d \n", userInput);
+	return 0;
+
+}
